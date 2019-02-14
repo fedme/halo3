d43d6e98d213c3822e252529687318fc12277376
Federico Meini <federico.meini@gmail.com>
2019-02-14 11:01:44 +0100

    Remove inferrable types

--- src/app/app.info.ts
+++ src/app/app.info.ts
@@ -1,5 +1,5 @@
 export namespace AppInfo {
-    export const id: string = '76499d07';
-    export const nameLabel: string = 'halo3';
-    export const version: string = '1.1.1';
+    export const id = '76499d07';
+    export const nameLabel = 'halo3';
+    export const version = '1.1.1';
 }
